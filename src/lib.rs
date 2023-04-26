f508003cb66318416c94fbc9f892c4ad0b2e6179
Ian Campbell <campbellni@hendrix.edu>
2023-04-26 16:47:33 -0500

    WORKING

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,20 @@
-#![cfg_attr(not(test), no_std)]
+//#![cfg_attr(not(test), no_std)]
 
 use gc_headers::{Tracer, HeapResult, Pointer, GarbageCollectingHeap, HeapError};
 
 #[derive(Copy, Clone, Debug)]
 pub struct CopyingHeap<const HEAP_SIZE: usize, const MAX_BLOCKS: usize> {
+    memory: [[u64; HEAP_SIZE]; 2],
+    new: usize,
+    old: usize,
+    num_blocks: usize,
+    next_available_block_index: usize,
+    pointer_buffer: [Pointer; MAX_BLOCKS],
+    block_num_buff: [usize; MAX_BLOCKS],
+    block_size_buff: [usize; MAX_BLOCKS],
+    block_alloc_buff: [bool; MAX_BLOCKS],
+
+    
     // YOUR CODE HERE
 }
 
@@ -11,19 +22,131 @@ impl<const HEAP_SIZE: usize, const MAX_BLOCKS: usize> GarbageCollectingHeap for
     CopyingHeap<HEAP_SIZE, MAX_BLOCKS>
 {
     fn new() -> Self {
-        todo!("Create the heap");
+        let mut memory = [[0 as u64; HEAP_SIZE]; 2];
+        let mut pointer_buffer = [Pointer::new(0, MAX_BLOCKS); MAX_BLOCKS];
+        let mut block_num_buff = [0; MAX_BLOCKS];
+        let mut block_size_buff = [0; MAX_BLOCKS];
+        let mut block_alloc_buff = [false; MAX_BLOCKS];
+        Self{memory, new: 1, old: 0, num_blocks: 0, next_available_block_index: 0, pointer_buffer, block_num_buff, block_size_buff, block_alloc_buff}
+
     }
 
     fn load(&self, p: Pointer) -> HeapResult<u64> {
-        todo!("Load a value from the heap");
+        return HeapResult::Ok(self.memory[self.old][self.block_num_buff[p.block_num()] + p.offset()]) ;
     }
 
     fn store(&mut self, p: Pointer, value: u64) -> HeapResult<()> {
-        todo!("Store a value in the heap");
+        if p.offset() < p.len(){
+            self.memory[self.old][self.block_num_buff[p.block_num()] + p.offset()] = value;
+            p.next();
+            return HeapResult::Ok(())
+        }
+        else{
+            return HeapResult::Err(HeapError::OffsetTooBig)
+        }
     }
 
     fn malloc<T: Tracer>(&mut self, num_words: usize, tracer: &T) -> HeapResult<Pointer> {
-        todo!("Allocate a new block in the heap, collecting if needed.");
+        //println!("{:?}", self.memory);
+        //if self.num_blocks < MAX_BLOCKS{
+            if num_words + self.next_available_block_index <= HEAP_SIZE{
+                if self.num_blocks==MAX_BLOCKS{
+                    return HeapResult::Err(HeapError::OutOfBlocks);
+                }
+                let mut block_number = 0;
+                for i in 0..MAX_BLOCKS{
+                    if !self.block_alloc_buff[i]{
+                        block_number = i;
+                        self.block_alloc_buff[i] = true;
+                        break;
+                    }
+                }
+                let p = Pointer::new(block_number, num_words);
+                self.block_num_buff[block_number] = self.next_available_block_index;
+                self.block_size_buff[block_number] = num_words;
+                self.num_blocks +=1;
+                self.next_available_block_index += num_words;
+                
+                self.pointer_buffer[block_number] = p;
+                
+                return HeapResult::Ok(p);
+            }
+            else {
+                self.num_blocks = 0;
+                self.block_alloc_buff = [false;MAX_BLOCKS];
+                let mut trace_buffer = [false; MAX_BLOCKS];
+                //println!("{:?}",trace_buffer);
+                tracer.trace(&mut trace_buffer);
+                
+                //println!("{:?}",trace_buffer);
+                //println!("BREAK");
+                self.memory[self.new] = [0;HEAP_SIZE];
+                let mut temp_p_buff = [Pointer::new(MAX_BLOCKS+7, MAX_BLOCKS); MAX_BLOCKS];
+                let mut new_index = 0;
+                for (i,val) in trace_buffer.iter().enumerate(){
+                    if *val{
+                        // let temp_index = new_index;
+                        // for j in self.block_num_buff[i]..self.block_num_buff[i]+self.block_size_buff[i]{
+                        //     self.memory[self.new][new_index] = self.memory[self.old][j];
+                        //     new_index +=1;
+                        // }
+                        // self.block_num_buff[i] = temp_index;
+                        // copied_blocks +=1;
+                        // self.num_blocks+=1;
+                        
+                        for j in 0..self.block_size_buff[i]{
+                            self.memory[self.new][new_index+j] = self.memory[self.old][j+self.block_num_buff[i]];
+                            //self.store(self.pointer_buffer[i], self.load(self.pointer_buffer[i]).unwrap());
+                        }
+                        self.block_num_buff[i] = new_index;
+                        self.num_blocks+=1;
+                        self.block_alloc_buff[i] = true;
+                        new_index += self.block_size_buff[i];
+                    }
+                }
+                
+                self.next_available_block_index = new_index;
+                let temp = self.old;
+                self.old = self.new;
+                self.new = temp;
+                self.memory[self.new] = [0;HEAP_SIZE];
+                println!("{:?}",self.memory);
+                println!("{}",new_index);
+                println!("{}",num_words);
+                // for i in 
+                if self.num_blocks==MAX_BLOCKS{
+                    return HeapResult::Err(HeapError::OutOfBlocks);
+                }
+                if num_words + self.next_available_block_index <= HEAP_SIZE{
+                    if self.num_blocks==MAX_BLOCKS{
+                        return HeapResult::Err(HeapError::OutOfBlocks);
+                    }
+                    let mut block_number = 0;
+                    for i in 0..MAX_BLOCKS{
+                        if !self.block_alloc_buff[i]{
+                            block_number = i;
+                            self.block_alloc_buff[i] = true;
+                            break;
+                        }
+                    }
+                    let p = Pointer::new(block_number, num_words);
+                    self.block_num_buff[block_number] = self.next_available_block_index;
+                    self.block_size_buff[block_number] = num_words;
+                    self.num_blocks +=1;
+                    self.next_available_block_index += num_words;
+                    
+                    self.pointer_buffer[block_number] = p;
+                    
+                    return HeapResult::Ok(p);
+                }
+                else {
+                    return  HeapResult::Err(HeapError::OutOfMemory);
+                }
+            }
+        // }   
+        // else{
+        //     return HeapResult::Err(HeapError::OutOfMemory);
+        // }
     }
 }
 
@@ -31,15 +154,15 @@ impl<const HEAP_SIZE: usize, const MAX_BLOCKS: usize>
     CopyingHeap<HEAP_SIZE, MAX_BLOCKS>
 {
     pub fn is_allocated(&self, block: usize) -> bool {
-        todo!("Return true if block is allocated, false otherwise");
+        return self.block_alloc_buff[block];
     }   
             
     pub fn num_allocated_blocks(&self) -> usize {
-        todo!("Return the number of blocks allocated and in use");
+        return self.num_blocks;
     }       
             
     pub fn size_of(&self, block: usize) -> usize {
-        todo!("Return the number of words in the given block");
+        return self.block_size_buff[block];
     }       
 
 }
@@ -57,24 +180,26 @@ mod tests {
             tracer.allocate_next(request, &mut allocator).unwrap();
         }
         assert_eq!(tracer.len(), allocator.num_allocated_blocks());
+        println!("{:?}",allocator);
         assert!(tracer.matches(&allocator));
+
         assert_eq!(tracer.total_allocated(), 94);
 
         match tracer.allocate_next(1, &mut allocator) {
             HeapResult::Ok(_) => panic!("Should be an error!"),
             HeapResult::Err(e) => assert_eq!(e, HeapError::OutOfBlocks),
         }
-
+        
         tracer.test_in_bounds(&mut allocator);
-
         for _ in 0..(tracer.len() / 2) {
             tracer.deallocate_next_even();
         }
+        
         assert!(tracer.matches(&allocator));
         assert_eq!(tracer.total_allocated(), 24);
 
         tracer.test_in_bounds(&mut allocator);
-
+        println!("{:?}",allocator);
         tracer.allocate_next(4, &mut allocator).unwrap();
         assert_eq!(tracer.len(), allocator.num_allocated_blocks());
 
@@ -139,6 +264,7 @@ mod tests {
             let p = allocator.malloc(1, self).unwrap();
             allocator.store(p, 1).unwrap();
             
+            println!("{:?}",MAX_BLOCKS);
             println!("looking up {:?}", self.count_ptr.unwrap());
             let count = allocator.load(self.count_ptr.unwrap()).unwrap();
             assert_eq!(count, self.counts);
